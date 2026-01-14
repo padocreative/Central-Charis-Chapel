b6ced354c226e555f1b0d3b330c90cef74a4cdf1
itcreativetbb <it.creative0@gmail.com>
2026-01-14 23:24:37 +0000

    Increase chunk size warning limit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increased limit to suppress warnings for large libs like react-player
+  }
 })
